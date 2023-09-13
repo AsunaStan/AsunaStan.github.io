f06ef6098da25bde2faaf03845794289e0371523
AsunaStan <144958411+AsunaStan@users.noreply.github.com>
2023-09-13 15:24:30 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,20 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import Organization from './hub';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Our Organization</h1>
+      </header>
+      <main>
+        <Organization />
+      </main>
+    </div>
+  );
+}
+
+export default App;
 
