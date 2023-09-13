6267c176b4393413d59c3c452d89cdf2924cfe3b
AsunaStan <144958411+AsunaStan@users.noreply.github.com>
2023-09-13 15:22:11 -0700

    Update hub.js

--- hub.js
+++ hub.js
@@ -1,1 +1,23 @@
 
+import React from 'react';
+
+function Organization() {
+  return (
+    <div className="organization">
+      <h1>Our Organization</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor
+        libero id mi volutpat, sed bibendum elit facilisis. Nullam id massa
+        tincidunt, vehicula elit sit amet, dictum libero.
+      </p>
+      <h2>Mission Statement</h2>
+      <p>
+        Nulla facilisi. Mauris nec tellus eget elit congue scelerisque. Cras
+        at ex lectus.
+      </p>
+    </div>
+  );
+}
+
+export default Organization;
+
